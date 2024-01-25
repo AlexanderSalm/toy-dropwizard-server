9660dd420a2a7b6a6dc19efd7f68cb8b7fb1c642
Alikay <asalm@ualberta.ca>
2024-01-24 17:45:32 -0700

    change

--- src/test/java/TestHello.java
+++ src/test/java/TestHello.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestHello {
-
+    //week 2 change
     @Test
     public void testFoo() {
         int result = 5 + 5 * 2;
